47b63000c92b324b6611cb7bd37eae4c31ed0391
Will Sheppard <nobody@example.com>
2018-04-26 17:01:36 +0100

    Convert to CSV, parse data & populate

--- web.pl
+++ web.pl
@@ -5,15 +5,36 @@ package Tasks;
 
 use Moose;
 use Storable;
+use DateTime;
 
 has 'file' => (
    is => 'ro',
    isa => 'Str',
-   default => 'data.storable',
+   #default => 'data.storable',
+   default => 'data.csv',
 );
 
 has 'data' => (
     is => 'rw',
+    isa => 'HashRef',
+);
+
+has 'datetime' => (
+    is => 'ro',
+    isa => 'DateTime',
+    lazy => 1,
+    default => sub {
+        return DateTime->now;
+    },
+);
+
+has 'day' => (
+    is => 'rw',
+    isa => 'Str',
+    default => sub {
+        my ($self) = @_;
+        return lc($self->datetime->day_abbr);
+    },
 );
 
 sub BUILD {
@@ -61,6 +82,66 @@ sub save {
 
 # Retrieve data from disk
 sub load {
+    my ($self) = @_;
+    use Text::CSV;
+    my $csv = Text::CSV->new or die "Cannot use CSV: ".Text::CSV->error_diag;
+    open my $fh, "<:encoding(utf8)", $self->file or die $self->file.": $!";
+    # Parse CSV into data structure
+    # TODO: Use a class for the data instead of a hash
+    my $headers = $csv->getline($fh);
+    $csv->column_names(@$headers);
+    my $data;
+    while (my $row = $csv->getline_hr($fh)) {
+        my $key = delete($row->{system_name});
+
+        # separate combined fields
+        foreach my $field (qw/when states/) {
+            $row->{$field} = [ split('/', delete($row->{$field})) ];
+        }
+
+        # active or not?
+        $row->{active} = (grep { lc($_) eq lc($self->day) } @{ $row->{when} }) ? 1 : 0;
+
+        # define next states
+        my $prev;
+        my $next_states = {};
+        foreach my $entry (@{ $row->{states} }) {
+            # make state chains
+            my $state1 = $entry; $state1 =~ s/=.+//;
+            if (! $prev) { # first state
+                $next_states->{start} = $state1;
+            }
+            else { # subsequent states
+                $next_states->{$prev} = $state1;
+            }
+            $prev = $state1;
+
+            # seperate combined fields
+            next if $entry !~ /=/;
+            my ($state, $next) = split('=', $entry);
+            $next_states->{$state} = "ext:".$next;
+        }
+        $next_states->{$prev} = 'finish' unless $next_states->{$prev};
+
+        # current state
+        $row->{current_state} = $next_states->{start};
+
+        # update states
+        $row->{states} = $next_states;
+
+        # populate data
+        $data->{tasks}{$key} = $row;
+    }
+use Data::Dumper;
+print "data = ".Dumper($data);
+    $csv->eof or $csv->error_diag;
+    close $fh;
+    $self->data($data);
+    return;
+}
+
+# Retrieve data from disk
+sub load_old {
     my ($self) = @_;
     my $file = $self->file;
     die error("expected file") unless $file;
@@ -168,11 +249,15 @@ sub web_display {
           $cgi->strong('Display'),
           $cgi->br;
 
-    print $cgi->p(
-        $data->{tasks}{$_}{display_name}
-        . ' => ' .
-        $data->{tasks}{$_}{state}
-    ) foreach keys %{ $data->{tasks} };
+    foreach my $key (keys %{ $data->{tasks} }) {
+        my $task = $data->{tasks}{$key};
+        next unless $task->{active};
+        print $cgi->p(
+            $task->{display_name}
+            . ' => ' .
+            $task->{current_state}
+        );
+    }
 
     print $cgi->end_html;
     return;
